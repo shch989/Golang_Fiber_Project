8c99d234f5e711f7f10b3cb067b8b279687426d4
shch989 <tankpop11@gmail.com>
2023-08-19 00:56:20 +0900

    Read

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,9 +1,20 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
 
-func UserHandlerRead(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"hello": "world!",
-	})
+	"github.com/gofiber/fiber/v2"
+	"github.com/shch989/Golang_Fiber_Project/database"
+	"github.com/shch989/Golang_Fiber_Project/models/entity"
+)
+
+func UserHandlerGetAll(c *fiber.Ctx) error {
+	var users []entity.User
+
+	err := database.DB.Find(&users).Error
+	if err != nil {
+		log.Println(err)
+	}
+
+	return c.JSON(users)
 }
--- routers/index.route.go
+++ routers/index.route.go
@@ -6,5 +6,5 @@ import (
 )
 
 func RouteInit(r *fiber.App) {
-	r.Get("/user", controllers.UserHandlerRead)
+	r.Get("/user", controllers.UserHandlerGetAll)
 }
