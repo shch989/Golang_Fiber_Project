6f09b82421c77b2cfde6fbb1cf9cb5f6ce4d752b
shch989 <tankpop11@gmail.com>
2023-08-18 21:37:18 +0900

    Json 출력 테스트

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"hello": "world!",
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
